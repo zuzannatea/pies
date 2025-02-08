46bee52d8baebdbc2024098ee0b6c1265b399c49
zuzannatea <zuziabibik@gmail.com>
2025-02-08 10:44:57 +0100

    part2Write to the Database

--- people.py
+++ people.py
@@ -1,30 +1,10 @@
-from datetime import datetime
 from flask import abort, make_response
-
-
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
-PEOPLE = {
-    "Fairy": {
-        "fname": "Tooth",
-        "lname": "Fairy",
-        "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    }
-}
+from config import db
+from models import Person, people_schema, person_schema
 
 def read_all():
-    return list(PEOPLE.values())
+    people = Person.query.all()
+    return people_schema.dump(people)
 
 def create(person):
     lname = person.get("lname")
@@ -44,12 +24,11 @@ def create(person):
         )
 
 def read_one(lname):
-    if lname in PEOPLE:
-        return PEOPLE[lname]
+    person = Person.query.filter(Person.lname == lname).one_or_none()
+    if person is not None:
+        return person_schema.dump(person)
     else:
-        abort(
-            404, f"Person with last name {lname} not found"
-        )
+        abort(404, f"Person with last name {lname} not found")
 
 def update(lname, person):
     if lname in PEOPLE:
